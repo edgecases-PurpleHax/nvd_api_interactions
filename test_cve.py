002a0ea6df3775ecb922b77ea9b77a8fa7cb8447
Restyled.io <commits@restyled.io>
2021-10-08 22:15:20 +0000

    Restyled by autopep8

--- test_cve.py
+++ test_cve.py
@@ -9,24 +9,27 @@ def get_test_file(file):
     with open(path, 'r') as infile:
         return infile.read()
 
+
 def test_get_cve_by_id(requests_mock: Mocker):
     cve_id = 'CVE-2021-3165'
     cve_data = get_test_file(f'{cve_id}.json')
-    requests_mock.get(f'https://services.nvd.nist.gov/rest/json/cve/1.0/{cve_id}', text=cve_data)
+    requests_mock.get(
+        f'https://services.nvd.nist.gov/rest/json/cve/1.0/{cve_id}', text=cve_data)
 
     result = get_cve_by_id(cve_id)
 
     assert requests_mock.called_once
     assert "cve@mitre.org" in result
 
+
 @pytest.mark.xfail(reason="Not implemented")
 def test_get_all_cves(requests_mock: Mocker, tmpdir):
     # todo: this is a placeholder test that currently is expected to fail.
 
     all_cves_response = get_test_file('all_cves.json')
-    requests_mock.get('https://services.nvd.nist.gov/rest/json/cves/1.0', text=all_cves_response)
+    requests_mock.get(
+        'https://services.nvd.nist.gov/rest/json/cves/1.0', text=all_cves_response)
 
     result = get_all_cves()
 
     assert requests_mock.called
-
