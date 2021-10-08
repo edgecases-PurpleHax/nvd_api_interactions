7dbef1fa36bd9a44204b7b6f57c99f0b525683e2
Restyled.io <commits@restyled.io>
2021-10-08 22:15:21 +0000

    Restyled by black

--- test_cve.py
+++ test_cve.py
@@ -5,16 +5,17 @@ import pytest
 
 
 def get_test_file(file):
-    path = os.path.join(os.path.dirname(__file__), f'testfiles/{file}')
-    with open(path, 'r') as infile:
+    path = os.path.join(os.path.dirname(__file__), f"testfiles/{file}")
+    with open(path, "r") as infile:
         return infile.read()
 
 
 def test_get_cve_by_id(requests_mock: Mocker):
-    cve_id = 'CVE-2021-3165'
-    cve_data = get_test_file(f'{cve_id}.json')
+    cve_id = "CVE-2021-3165"
+    cve_data = get_test_file(f"{cve_id}.json")
     requests_mock.get(
-        f'https://services.nvd.nist.gov/rest/json/cve/1.0/{cve_id}', text=cve_data)
+        f"https://services.nvd.nist.gov/rest/json/cve/1.0/{cve_id}", text=cve_data
+    )
 
     result = get_cve_by_id(cve_id)
 
@@ -26,9 +27,10 @@ def test_get_cve_by_id(requests_mock: Mocker):
 def test_get_all_cves(requests_mock: Mocker, tmpdir):
     # todo: this is a placeholder test that currently is expected to fail.
 
-    all_cves_response = get_test_file('all_cves.json')
+    all_cves_response = get_test_file("all_cves.json")
     requests_mock.get(
-        'https://services.nvd.nist.gov/rest/json/cves/1.0', text=all_cves_response)
+        "https://services.nvd.nist.gov/rest/json/cves/1.0", text=all_cves_response
+    )
 
     result = get_all_cves()
 
