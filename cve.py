799a084d3b1f1ad8ca6c542fbdcee88706dd2497
Restyled.io <commits@restyled.io>
2021-10-08 21:27:07 +0000

    Restyled by autopep8

--- cve.py
+++ cve.py
@@ -10,6 +10,7 @@ import urllib
 # todo: This should be done with argparse at first. Make it work with CLI then eventually figure out how to make it
 #  work with Flask to make a pretty dashboard or some shit.
 
+
 class MyParser(argparse.ArgumentParser):
     def error(self, message):
         sys.stderr.write(f'error: {message}\n')
@@ -60,9 +61,11 @@ def get_cve_after_date(start_date):
     if r.json()['totalResults'] > 20:
         page = 0
         while page < r.json()['totalResults']:
-            print(f"There are {r.json().get('totalResults')} total results. The results will be paginated")
-            next_page = requests.get(f'https://services.nvd.nist.gov/rest/json/cves/1.0?startIndex={page}&pubStartDate={start_date}T00:00:00:000 UTC-05:00')
-            if page==0:
+            print(
+                f"There are {r.json().get('totalResults')} total results. The results will be paginated")
+            next_page = requests.get(
+                f'https://services.nvd.nist.gov/rest/json/cves/1.0?startIndex={page}&pubStartDate={start_date}T00:00:00:000 UTC-05:00')
+            if page == 0:
                 print(f'This is page #{page+1}')
             else:
                 print(f'This is page #{int(page/20+1)}')
@@ -80,14 +83,16 @@ def get_cve_after_date(start_date):
 def get_cve_between(start_date, end_date):
     params = {
         "pubStartDate": f'{start_date}T00:00:00:000 UTC-05:00',
-        "pubEndDate":f'{end_date}T00:00:00:000 UTC-05:00'
+        "pubEndDate": f'{end_date}T00:00:00:000 UTC-05:00'
     }
-    r = requests.get(url="https://services.nvd.nist.gov/rest/json/cves/1.0", params=urllib.parse.urlencode(params))
+    r = requests.get(url="https://services.nvd.nist.gov/rest/json/cves/1.0",
+                     params=urllib.parse.urlencode(params))
     if r.json()['totalResults'] > 20:
         page = 0
         while page < r.json()['totalResults']:
             if page == 0:
-                print(f"There are {r.json().get('totalResults')} total results. The results will be paginated")
+                print(
+                    f"There are {r.json().get('totalResults')} total results. The results will be paginated")
             next_page = requests.get(
                 f'https://services.nvd.nist.gov/rest/json/cves/1.0?startIndex={page}&pubStartDate={start_date}T00:00:00:000 UTC-05:00')
             if page == 0:
@@ -105,7 +110,8 @@ def get_cve_between(start_date, end_date):
 
 
 def get_cve_by_id(cve_id):
-    r = requests.get(f'https://services.nvd.nist.gov/rest/json/cve/1.0/{cve_id}')
+    r = requests.get(
+        f'https://services.nvd.nist.gov/rest/json/cve/1.0/{cve_id}')
     return format_cve_information(r.json())
 
 
@@ -119,9 +125,11 @@ def get_all_cves():
         if r.json()['totalResults'] > 20:
             page = 0
             while page < r.json()['totalResults']:
-                print(f"There are {r.json().get('totalResults')} total results. The results will be paginated")
-                next_page = requests.get(f'https://services.nvd.nist.gov/rest/json/cves/1.0?startIndex={page}')
-                if page==0:
+                print(
+                    f"There are {r.json().get('totalResults')} total results. The results will be paginated")
+                next_page = requests.get(
+                    f'https://services.nvd.nist.gov/rest/json/cves/1.0?startIndex={page}')
+                if page == 0:
                     print(f'This is page #{page+1}')
                 else:
                     print(f'This is page #{int(page/20+1)}')
@@ -142,6 +150,7 @@ def get_all_cves():
     else:
         sys.exit()
 
+
 def format_existing_json(file):
     to_format = {}
     with open(file, 'r') as f:
@@ -156,18 +165,22 @@ def parse_args():
     parser.add_argument("-a", "--all", action="store_true", help="Use this only one time. It will write a file with the"
                                                                  "entire NVD database")
     parser.add_argument("-i", "--get-by-id", action="store_true", help="Requires -I/--ID <CVE ID>. Gets information "
-                                                                        "about CVE ID provided")
-    parser.add_argument("-I", "--ID", action="store", help="Enter CVE ID in the format CVE-2021-3165.")
+                        "about CVE ID provided")
+    parser.add_argument("-I", "--ID", action="store",
+                        help="Enter CVE ID in the format CVE-2021-3165.")
     parser.add_argument("-b", "--between-dates", action="store_true", help="Requires -S/--Start-Date <Start Date> "
                                                                            "and -E/--End-Date <End Date>,gets all "
                                                                            "CVEs between the start and end date")
-    parser.add_argument("-S", "--Start-Date", action="store", help="Enter in the format YYYY-MM-DD")
-    parser.add_argument("-E", "--End-Date", action="store", help="Enter in the format YYYY-MM-DD")
+    parser.add_argument("-S", "--Start-Date", action="store",
+                        help="Enter in the format YYYY-MM-DD")
+    parser.add_argument("-E", "--End-Date", action="store",
+                        help="Enter in the format YYYY-MM-DD")
     parser.add_argument("-A", "--After-Date", action="store_true", help="Requires -S/--Start-Date <start date>. Gets"
                                                                         " all CVE from Start date to current date")
     parser.add_argument("-f", "--format", action="store_true", help="Requires -F/--File <file name>. Formats an existing json file"
                                                                     "from NVD API")
-    parser.add_argument("-F", "--File", action="store", help="Enter file name to format")
+    parser.add_argument("-F", "--File", action="store",
+                        help="Enter file name to format")
     args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
     return args
 
@@ -186,7 +199,8 @@ if __name__ == "__main__":
     # Get CVE between start and end date
     if args.between_dates:
         if not args.Start_Date and args.End_Date:
-            print('[-] You must enter a start date and end date. Enter --help for example.')
+            print(
+                '[-] You must enter a start date and end date. Enter --help for example.')
         else:
             get_cve_between(args.Start_Date, args.End_Date)
     # Get CVEs after Start date
