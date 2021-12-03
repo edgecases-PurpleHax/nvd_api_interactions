783bb32df67e591bd204d7043bc83fbe6c8f4573
Ryan Wilson <rwils83@gmail.com>
2021-12-03 14:32:08 -0500

    fixed a thing

--- report_parser.py
+++ report_parser.py
@@ -1,16 +1,20 @@
 import csv
+import sys
 cves = []
-with open('vuln_reports/vuln_report_99941_Dec 03 2021_11_36 (EST).csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            line_count += 1
-            pass
-        else:
-            if row[0] not in cves:
-                cves.append(row[0])
+if len(sys.argv) == 2:
+    with open(sys.argv[1], 'r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+                pass
+            else:
+                if row[0] not in cves:
+                    cves.append(row[0])
 
-with open('parsed_data', 'w') as file:
-    for cve in cves:
-        file.write(cve+"\r")
+    with open('parsed_data', 'w') as file:
+        for cve in cves:
+            file.write(cve+"\r")
+else:
+    print("[-] Usage: python3 report_parser.py <filename>")
